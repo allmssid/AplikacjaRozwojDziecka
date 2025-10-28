dea7a9247896920fc9e1b20ba16d039b9f1e6e7b
olass <olass@MSSID>
2025-10-28 18:20:27 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplikacjaRozwojDziecka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
